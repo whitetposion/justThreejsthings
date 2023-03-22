08cfbb89a1a1148f0a411ed0607d6bb8499c27fc
abhishek <purohitabhishek308@gmail.com>
2023-03-22 23:42:21 +0530

    day2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,24 @@ import { logo  ,menu , close} from '../assets'
 const Navbar = () => {
   const [active, setActive] = useState("")
   return (
-    <nav className={`${styles.paaddingX} W-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-betweeen items-center max-w-7xl mx-auto">
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to='/' className='flex items-center gap-2' onClick={()=> {setActive(""); window.scrollTo(0,0)
           }}>
             <img src={logo} alt='logo' className="w-9 h-9 object-contain"/>
+            <p className="text-white text-[18px] font-bold cursor-pointer">Abhishek
+            <span className=" sm:block hidden">Purohit</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id} className ={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer `} onClick ={()=> setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src= {menu}/>
+        </div>
       </div>
     </nav>
   )
